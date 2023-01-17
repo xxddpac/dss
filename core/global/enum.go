b643527a67e74b58bc93aee6e3bd71aaed51a9e4
大米 <xxddpac@gmail.com>
2023-01-17 19:06:43 +0800

    feat: modify rule models

--- core/global/enum.go
+++ core/global/enum.go
@@ -6,3 +6,24 @@ const (
 	Consumer RunMode = iota
 	Producer
 )
+
+type RuleType int
+
+const (
+	Single RuleType = iota + 1
+	Range
+	Cidr
+)
+
+func (r RuleType) String() string {
+	switch r {
+	case Single:
+		return "单个IP类型" //192.168.1.1
+	case Range:
+		return "连续范围IP类型" //192.168.1.1-192.168.1.100
+	case Cidr:
+		return "网段类型" //192.168.1.0/24
+	default:
+		return "未知"
+	}
+}
--- core/models/rule.go
+++ core/models/rule.go
@@ -2,14 +2,16 @@ package models
 
 import (
 	"github.com/globalsign/mgo/bson"
+	"goportscan/core/global"
 	"time"
 )
 
 type Rule struct {
-	Name       string `json:"name" bson:"name" binding:"required"`
-	Status     bool   `json:"status" bson:"status" binding:"required"`
-	TargetHost string `json:"target_host" bson:"target_host" binding:"required"`
-	TargetPort string `json:"target_port" bson:"target_port" binding:"required"`
+	Name       string          `json:"name" bson:"name" binding:"required"`
+	Status     bool            `json:"status" bson:"status" binding:"required"`
+	Type       global.RuleType `json:"type" bson:"type" binding:"required"`
+	TargetHost string          `json:"target_host" bson:"target_host" binding:"required"`
+	TargetPort string          `json:"target_port" bson:"target_port" binding:"required"`
 }
 
 type RuleInsert struct {
